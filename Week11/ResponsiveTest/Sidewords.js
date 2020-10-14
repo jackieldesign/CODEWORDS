6a0b6cb4d9b835bc4c951e5cdeeee5542d582770
Jackie Liu <63223748+jackieliiu@users.noreply.github.com>
2020-10-14 22:58:31 +1100

    Add files via upload

---
+++ Week11/ResponsiveTest/Sidewords.js
@@ -0,0 +1,21 @@
+class Sidewords {
+  constructor (x,y,phrase,size,align) {
+    this.x = x;
+    this.y = y;
+    this.speed = 4;
+    this.text = phrase;
+    this.textSize = size;
+    this.align = align;
+  }
+  move () {
+    this.x += random(-this.speed,this.speed);
+    //this.y += random(this.speed,this.speed);
+  }
+  show () {
+    fill (0);
+    textAlign(this.align);
+    textFont(font2);
+    textSize(this.textSize);
+    text(this.text, this.x, this.y);
+  }
+}
